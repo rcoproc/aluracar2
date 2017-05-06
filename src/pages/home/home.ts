82587aa1582994319f090840ee5303a8de23b00f
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-05-06 14:41:55 -0400

    Seleção do carro para o Agendamento

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
@@ -42,5 +43,10 @@ export class HomePage implements OnInit {
               subTitle: 'Não foi possível obter a lista de carros. Tente mais tarde.'
           }).present();
       });
+  }
+
+  seleciona(carro) {
+
+    this.navCtrl.push(EscolhaPage, { carroSelecionado: carro } );
   }
 }
