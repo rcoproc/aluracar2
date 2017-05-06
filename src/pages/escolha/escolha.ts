14d007e5558a4c2784f911160d714c841eed26ed
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-05-06 16:14:12 -0400

    Acessórios pela lista e botão Toogle

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -9,10 +9,15 @@ import { NavParams } from 'ionic-angular';
 export class EscolhaPage {
 
     public carro;
+    public acessorios;
 
     constructor(public navParams: NavParams) {
         this.carro = this.navParams.get('carroSelecionado');
-        console.log(this.carro.nome);
+        this.acessorios = [
+            { nome: 'Freio ABS', preco: 800 },
+            { nome: 'Ar-condicionado', preco: 1000 },
+            { nome: 'MP3 Player', preco: 500 }
+        ];
     }
 
 }
