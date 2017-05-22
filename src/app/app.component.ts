13ea7dca8447b639f83cc401d0ac2aa9596938a4
Ricardo Cesar de Oliveira <rcoproc@gmail.com>
2017-05-21 23:37:23 -0400

    SidebarMenu e Inicio tela agendamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { AgendamentosPage } from '../pages/agendamentos/agendamentos';
 
 
 @Component({
@@ -11,6 +12,12 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
+  public paginas = [
+    { titulo: 'Agendamento', componente: AgendamentosPage }
+  ];
+
+  @ViewChild(Nav) public nav: Nav;
+
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -18,5 +25,9 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  abrePagina(pagina) {
+    this.nav.push(pagina.componente);
   }
 }
--- src/domain/agendamento/agendamento-dao.ts
+++ src/domain/agendamento/agendamento-dao.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Agendamento } from '../../domain/agendamento/agendamento';
 import { Storage } from '@ionic/storage';
+import { Carro } from '../../domain/carro/carro';
 
 @Injectable()
 export class AgendamentoDao {
@@ -26,5 +27,25 @@ export class AgendamentoDao {
                 return dado ? true : false
             });
 
+    }
+
+    listaTodos() {
+
+        let agendamentos = [];
+
+        return this._storage.forEach(dado =>{
+
+            let carro = new Carro(dado.carro.nome, dado.carro.preco);
+
+            let agendamento = new Agendamento(
+                carro, 
+                dado.valor,
+                dado.nome,
+                dado.endereco,
+                dado.email,
+                dado.data,
+                dado.confirmado);
+            agendamentos.push(agendamento);
+        }).then(() => agendamentos);
     }
 }
